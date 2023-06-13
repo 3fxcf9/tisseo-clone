abc5f0ae14d7381b8aa60e89e02fff64ab055a64
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2023-06-13 18:25:09 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.3fxcf9.tclone',
+  appName: 'tisseo-clone',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
