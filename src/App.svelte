c75d4f8a64eb181d741183eda72057e7b67fd509
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2023-06-19 00:16:58 +0200

    Add navigation

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,24 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+	import { Router, Route, Link } from "svelte-routing";
+	import TabBar from "./lib/TabBar.svelte";
+	export let url = "";
+
+	// Pages
+	import Home from "./pages/Home.svelte";
+	import Lines from "./pages/Lines.svelte";
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<Router {url}>
+	<main>
+		<Route path="lines" component={Lines} />
+		<Route path="/"><Home /></Route>
+	</main>
 
-  <div class="card">
-    <Counter />
-  </div>
+	<TabBar />
+</Router>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+<style scoped>
+	main {
+		padding: 2rem;
+	}
 </style>
---
+++ src/lib/TabBar.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import { Link } from "svelte-routing";
+
+	// Icons
+	import HomeIcon from "./icons/home.svelte";
+	import LinesIcon from "./icons/lines.svelte";
+</script>
+
+<nav>
+	<Link class="tab-link" to="/">
+		<HomeIcon />
+	</Link>
+	<Link class="tab-link" to="lines">
+		<LinesIcon />
+	</Link>
+</nav>
+
+<style>
+	nav {
+		border-top: 1px solid var(--on-background);
+		height: 4.5rem;
+		padding: 1.2rem 0;
+		display: flex;
+		align-items: stretch;
+		justify-content: space-around;
+	}
+	:global(a) {
+		font-size: 1rem;
+		color: var(--on-background);
+		flex: 1;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		position: relative;
+	}
+	:global(svg) {
+		height: 100%;
+		font-size: 1rem;
+	}
+	:global(.tab-link[aria-current="page"]) {
+		color: var(--background);
+	}
+	:global(.tab-link[aria-current="page"]):hover {
+		color: var(--background);
+	}
+	:global(.tab-link[aria-current="page"])::after {
+		content: "";
+		position: absolute;
+		background-color: var(--on-background);
+		padding: 1rem 2rem;
+		border-radius: 1rem;
+		z-index: -1;
+	}
+</style>
