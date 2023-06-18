a64602466880cba562ef5b5c76c5f53ff939ef74
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2023-06-19 01:06:28 +0200

    WIP: Lines tab

--- src/pages/Lines.svelte
+++ src/pages/Lines.svelte
@@ -1,1 +1,20 @@
+<script>
+	import { CapacitorHttp } from "@capacitor/core";
+	import getEndpointUrl from "../config/endpoints";
+
+	let lines = [];
+
+	getEndpointUrl("lines").then((url) => {
+		if (!url) return;
+		CapacitorHttp.get({ url }).then((response) => {
+			lines = response.data.lines.line;
+		});
+	});
+</script>
+
 <h1>Lines</h1>
+<ul>
+	{#each lines as line}
+		<li style="--color: {line.bgXmlColor}">{line.shortName}</li>
+	{/each}
+</ul>
