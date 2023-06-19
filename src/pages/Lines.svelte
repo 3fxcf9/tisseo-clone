f9c0b8708715bb2a813e34974ad85755c3c5bf3c
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2023-06-19 11:00:01 +0200

    Update lines list

--- src/pages/Lines.svelte
+++ src/pages/Lines.svelte
@@ -15,6 +15,41 @@
 <h1>Lines</h1>
 <ul>
 	{#each lines as line}
-		<li style="--color: {line.bgXmlColor}">{line.shortName}</li>
+		<li style="--line-color: {line.bgXmlColor}">
+			<span class="line__shortName">{line.shortName}</span>
+			<span class="line__destinations">{line.name}</span>
+		</li>
 	{/each}
 </ul>
+
+<style>
+	ul {
+		list-style-type: none;
+	}
+	li {
+		border-left: 5px solid var(--line-color);
+		padding: 0.5rem 1rem;
+
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		gap: 5rem;
+	}
+	li:not(:last-of-type) {
+		margin-bottom: 1rem;
+	}
+
+	.line__shortName {
+		font-size: 1.6rem;
+		font-weight: 600;
+	}
+
+	.line__destinations {
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+
+		text-transform: uppercase;
+		color: var(--on-background-dark);
+	}
+</style>
